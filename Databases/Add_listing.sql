f413197fbae361f5d93c2443e5c8df69c4e49a9d
Sanket-14 <144524849+Sanket-14@users.noreply.github.com>
2023-11-20 22:15:36 +0530

    Add files via upload

---
+++ Databases/Add_listing.sql
@@ -0,0 +1,14 @@
+
+-- create
+CREATE TABLE Add_listing (
+  comp_name VARCHAR(255) PRIMARY KEY ,
+  Region TEXT NOT NULL,
+  Country TEXT NOT NULL,
+  City TEXT NOT NULL,
+  Website TEXT NOT NULL,
+  Supplier_type TEXT NOT NULL,
+  Categories TEXT NOT NULL,
+  PV_capacity TEXT NOT NULL,
+  image_url TEXT 
+);
+
